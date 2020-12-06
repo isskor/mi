cf8f0a44fffb2df0e79cfbc92f9735abc618eacf
isskor <jphuong91@gmail.com>
2020-12-06 16:45:46 +0100

    ready

--- merinfo.js
+++ merinfo.js
@@ -4,16 +4,16 @@ const fs = require("fs-extra");
 // const EDGE_PATH = edgePaths.getEdgePath();
 (async () => {
   const browser = await chromium.launch({
-    headless: false,
+    headless: true,
   });
   const context = await browser.newContext();
 
   // WRITE INPUT FILE
-  const inputFile = "20ktill16k.csv";
+  const inputFile = "1mtill2m.csv";
   // WRITE START NUMBER FROM INPUT FILE  ************
-  const orgStartNr = 1;
+  const orgStartNr = 273;
   // WRITE OUTPUT NAME  ***************
-  const outFileName = "merinfo2.csv";
+  const outFileName = "outputFile.csv";
 
   let loops = 0;
   const maxLoops = 100;
@@ -27,11 +27,11 @@ const fs = require("fs-extra");
   await page.goto(url);
   // await page.setDefaultTimeout(10000);
 
-  await fs.writeFile(
-    outFileName,
-    "OrgNumber; PhoneNumber; Operator \n",
-    "utf8"
-  );
+  //   await fs.writeFile(
+  //     outFileName,
+  //     "OrgNumber; PhoneNumber; Operator \n",
+  //     "utf8"
+  //   );
 
   await page.click(
     "#qc-cmp2-ui > div.qc-cmp2-footer.qc-cmp2-footer-overlay.qc-cmp2-footer-scrolled > div > button.sc-ifAKCX.hEtmUQ"
@@ -70,38 +70,41 @@ const fs = require("fs-extra");
         await page.click(visaAllaTel);
 
         const phoneTable = "#phonetable > tbody";
-        await page.waitForTimeout(500);
+        await page.waitForTimeout(1000);
         await page.waitForSelector(phoneTable);
 
         const phoneNumbers = await page.$$(`${phoneTable} > tr`);
 
         phoneNumbers.map(async (number) => {
-          const operatorSelector = await number.$("td:nth-child(3)");
-          const operator = await page.evaluate(
-            (op) => op.innerText,
-            operatorSelector
-          );
-          // if (!/\b(\w*telia\w*)\b/gi.test(operator)) return;
+          try {
+            const operatorSelector = await number.$("td:nth-child(3)");
+            const operator = await page.evaluate(
+              (op) => op.innerText,
+              operatorSelector
+            );
+            // if (!/\b(\w*telia\w*)\b/gi.test(operator)) return;
 
-          const phoneNumSelector = await number.$("td:nth-child(1)");
-          // const phoneNumUserSelector = await number.$("td:nth-child(2)");
+            const phoneNumSelector = await number.$("td:nth-child(1)");
+            // const phoneNumUserSelector = await number.$("td:nth-child(2)");
 
-          const phoneNum = await page.evaluate(
-            (ph) => ph.innerText,
-            phoneNumSelector
-          );
-          // const phoneUserNum = await page.evaluate(
-          //   (ph) => ph.innerText,
-          //   phoneNumUserSelector
-          // );
+            const phoneNum = await page.evaluate(
+              (ph) => ph.innerText,
+              phoneNumSelector
+            );
+            // const phoneUserNum = await page.evaluate(
+            //   (ph) => ph.innerText,
+            //   phoneNumUserSelector
+            // );
 
-          console.log(phoneNum, operator);
-          await fs.appendFile(
-            outFileName,
-            `"${orgNumber}"; "${phoneNum}"; "${operator}"\n`
-          );
+            console.log(phoneNum, operator, loops);
+            await fs.appendFile(
+              outFileName,
+              `"${orgNumber}"; "${phoneNum}"; "${operator}"\n`
+            );
+          } catch (err) {}
         });
       } catch (err) {}
+      console.log(loops);
       if (loops > maxLoops) {
         await browser.close();
         break;
